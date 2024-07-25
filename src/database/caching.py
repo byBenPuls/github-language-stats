4e3fcd62630d80cf97daf289b79b93f61bf61f81
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-25 10:16:09 +0500

    added caching

---
+++ src/database/caching.py
@@ -0,0 +1,22 @@
+import time
+
+from src.database.redis import RedisPool
+
+redis = RedisPool()
+
+
+async def in_cache(key: str) -> bool:
+    conn = await redis.get_connection()
+    return bool(await conn.lrange(key, 0, -1))
+
+
+async def record_in_cache(key: str, *values) -> None:
+    conn = await redis.get_connection()
+    await conn.lpush(key, *values)
+    await conn.expireat(key, int(time.time()) + 60 * 60)
+
+
+async def get_from_cache(key: str) -> list:
+    conn = await redis.get_connection()
+    values = (await conn.lrange(key, 0, -1))[::-1]
+    return values
---
+++ src/database/redis.py
@@ -0,0 +1,23 @@
+import logging
+import typing
+
+import redis.asyncio as redis
+
+logger = logging.getLogger("uvicorn.error")
+
+
+class RedisPool:
+    def __init__(self) -> None:
+        self.pool = None
+
+    async def create_connection(self) -> None:
+        logger.debug('Trying to connect..')
+        client = redis.ConnectionPool()
+        self.pool = redis.Redis.from_pool(client)
+        logging.debug('Successfully connected!')
+
+    async def get_connection(self) -> typing.Optional[redis.Redis]:
+        return self.pool
+
+    async def close_connection(self):
+        self.pool.aclose()
