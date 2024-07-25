b808798c007f1eac866442b00ea32f229eadd1cf
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-26 00:21:39 +1000

    added svg response

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
+from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
@@ -31,10 +32,18 @@ app.mount("/src/static", StaticFiles(directory=static_dir, html=True), name="sta
 
 templates = Jinja2Templates(directory='src/static')
 
+from src.svg.card import UserData
+
 
 @app.get('/')
-async def main_page(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+async def main_page(username: str = None):
+    if username is None:
+        return RedirectResponse('https://github.com/byBenPuls')
+    card = await UserData(username).card()
+    return HTMLResponse(content=card, status_code=200, media_type='image/svg+xml')
+
+    # return templates.TemplateResponse('index.html',
+    #                                   media_type='image/svg+xml', context={'request': request})
 
 
 @app.post('/')
