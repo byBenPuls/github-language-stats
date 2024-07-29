41a0a8f1f3106f46681025d48c9a132899fb63c9
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-29 07:16:06 +0500

    optimized themes

--- src/svg/themes.py
+++ src/svg/themes.py
@@ -52,34 +52,30 @@ def css(header_fill: str = '#2f80ed',
     return style
 
 
-def create_background(x: str = '0.5', y: str = '0.5', rx: str = '4.5',
-                      height: str = '99', stroke: str = '#e4e2e2',
-                      width: str = '299', fill: str = '#fffefe', attrib: dict = None):
+def background(x: str = '0.5', y: str = '0.5', rx: str = '4.5',
+               height: str = '99', stroke: str = '#e4e2e2',
+               width: str = '299', fill: str = '#fffefe', attrib=None):
+    if attrib is None:
+        attrib = {}
     return Et.Element('rect', xmlns="http://www.w3.org/2000/svg", x=x, y=y,
                       rx=rx, height=f"{height}%", stroke=stroke, width=width, fill=fill,
                       attrib=attrib)
 
 
 class BaseTheme:
-    def __init__(self, *elements):
-        self.elements = elements
-        self.root = Et.Element("svg", xmlns="http://www.w3.org/2000/svg", width="300",
-                               height="140", viewBox="0 0 300 140", fill="none", role="img")
-        self.root.append(css(
-            header_fill='#fff',
-            lang_name_fill='#fff',
-            stat_fill='#fff'
-        ))
-
-    def background(self):
-        raise NotImplementedError
+    def __init__(self, width: str):
+        self.width = str(int(width) - 1)
+        self.root = []
 
     def insert(self):
-        for i in self.elements:
-            self.root.append(i)
+        self.root.append(css(
+            header_fill='#2f80ed',
+            lang_name_fill='#434d58',
+            stat_fill='#434d58'
+        ))
+        self.root.append(background(width=self.width))
 
     def card(self):
-        self.background()
         self.insert()
         return self.root
 
@@ -103,87 +99,123 @@ class GradientGenerator:
 
 
 class Main(BaseTheme):
-    def __init__(self, *elements) -> None:
-        super().__init__(*elements)
+    def __init__(self, width) -> None:
+        super().__init__(width)
         self.root.append(css())
-
-    def background(self):
-        self.root.append(create_background())
 
 
 class Gradient(BaseTheme):
-    def background(self):
-        defs = Et.Element('defs', xmlns="http://www.w3.org/2000/svg")
-        self.root.append(defs)
-        linear_gradient = Et.Element('linearGradient', id="gradient", gradientTransform="rotate(30)",
-                                     gradientUnits="userSpaceOnUse")
-        defs.append(linear_gradient)
-        stop1 = Et.Element('stop', offset="0%", attrib={'stop-color': "#e96443"})
-        stop2 = Et.Element('stop', offset="100%", attrib={'stop-color': "#904e95"})
-        linear_gradient.append(stop1)
-        linear_gradient.append(stop2)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': '1'}))
+    def __init__(self, width) -> None:
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'))
+
+    def insert(self):
+        self.root.append(GradientGenerator(0, (5, '#e96443'), (95, '#904e95')).generate())
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': '1'}))
 
 
 class Dark(BaseTheme):
-    def __init__(self, *elements):
-        super().__init__(*elements)
+    def __init__(self, width) -> None:
+        super().__init__(width)
         self.root.append(css(
             header_fill='#58A6FF',
             lang_name_fill='#C3D1D9',
             stat_fill='#C3D1D9'
         ))
 
-    def background(self):
-        self.root.append(create_background(fill="#0D1117", attrib={'stroke-opacity': '1'}))
+    def insert(self):
+        self.root.append(background(width=self.width, fill="#0D1117", attrib={'stroke-opacity': '1'}))
 
 
 class Monokai(BaseTheme):
-    def __init__(self, *elements):
-        super().__init__(*elements)
+    def __init__(self, width):
+        super().__init__(width)
         self.root.append(css(
             header_fill='#eb1f6a',
             lang_name_fill='#DEE2E4',
             stat_fill='#DEE2E4'
         ))
 
-    def background(self):
-        self.root.append(create_background(fill="#272822", attrib={'stroke-opacity': '1'}))
+    def insert(self):
+        self.root.append(background(width=self.width, fill="#272822", attrib={'stroke-opacity': '1'}))
 
 
 class AmbientGradient(BaseTheme):
-    def background(self):
+    def __init__(self, width):
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'
+        ))
+
+    def insert(self):
         gradient = GradientGenerator(35,
                                      (0, '#4158d0'),
                                      (50, '#c850c0'),
                                      (100, '#ffcc70')).generate()
         self.root.append(gradient)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
 
 
 class OceanBlueGradient(BaseTheme):
-    def background(self):
+    def __init__(self, width):
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'
+        ))
+
+    def insert(self):
         gradient = GradientGenerator(35, (0, '#2E3192'), (100, '#1BFFFF')).generate()
         self.root.append(gradient)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
 
 
 class EternalConstanceGradient(BaseTheme):
-    def background(self):
+    def __init__(self, width):
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'
+        ))
+
+    def insert(self):
         gradient = GradientGenerator(0, (5, '#09203F'), (95, '#537895')).generate()
         self.root.append(gradient)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
 
 
 class ViceCityGradient(BaseTheme):
-    def background(self):
+    def __init__(self, width):
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'
+        ))
+
+    def insert(self):
         gradient = GradientGenerator(0, (5, '#3494e6'), (95, '#ec6ead')).generate()
         self.root.append(gradient)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
 
 
 class PurpinkGradient(BaseTheme):
-    def background(self):
+    def __init__(self, width):
+        super().__init__(width)
+        self.root.append(css(
+            header_fill='#fff',
+            lang_name_fill='#fff',
+            stat_fill='#fff'
+        ))
+
+    def insert(self):
         gradient = GradientGenerator(0, (5, '#7f00ff'), (95, '#e100ff')).generate()
         self.root.append(gradient)
-        self.root.append(create_background(fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
+        self.root.append(background(width=self.width, fill="url(#gradient)", attrib={'stroke-opacity': "1"}))
