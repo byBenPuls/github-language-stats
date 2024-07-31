5afe6daf3c05f5df23e41c895d43df22ed55974f
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2024-07-31 09:47:39 +0500

    double quotes

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,30 +1,46 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
 from starlette.responses import Response, RedirectResponse, HTMLResponse
 
+from src.container import get_container
+from src.database.redis import Redis
 from src.svg import themes
 from src.svg.card import UserData
+from src.database.caching import CachedProgramLangRepo
+from src.github.repos import ProgramLangRepo
 
 router = APIRouter()
 
 
-@router.get('/')
-async def main_page(username: str | None = None, theme: str = 'main') -> Response:
-    # TODO: обчыно так не делают. Импортят Query и пишут descriprion там.
-    # TODO: Так же как и с возвратным значением. В свагер автоматом подтянется
-    """
-    Main page
-    :param theme: Card background theme
-    :param username: GitHub username
-    """
+def get_db():
+    return get_container().resolve(Redis)
+
+
+@router.get("/", description="Returns SVG card with programming languages stats")
+async def main_page(
+    username: str | None = Query(
+        default=None,
+        description="Github username. If not set, redirects to https://github.com/byBenPuls",
+    ),
+    theme: str = Query(
+        title="Theme",
+        default="main",
+        description="Background theme name",
+        examples=list(themes.keys()),
+    ),
+    db=Depends(get_db),
+) -> Response:
     if theme not in themes:
-        theme = 'main'
+        theme = "main"
 
     if username is None:
-        return RedirectResponse('https://github.com/byBenPuls')
+        return RedirectResponse("https://github.com/byBenPuls")
     # TODO: у тебя обработка логики и сборка свг файла происходит в одном месте. Надо делить
     # cart = await GetLangCart().execute(username)
+    languages = await CachedProgramLangRepo(db, ProgramLangRepo()).fetch_lang(username)
     # cart_svg = CartSVGBuilder().build()
-    card = await UserData(username, theme).card()
-    return HTMLResponse(content=card, status_code=HTTPStatus.OK, media_type='image/svg+xml')
+    card = await UserData(languages, theme).card()
+    return HTMLResponse(
+        content=card, status_code=HTTPStatus.OK, media_type="image/svg+xml"
+    )
