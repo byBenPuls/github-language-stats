865208ed82256d2b0e6017f7626a5f147d8605c8
Ben <discordben7@gmail.com>
2024-08-01 18:14:09 +0500

    typing modified

--- src/database/caching.py
+++ src/database/caching.py
@@ -12,11 +12,11 @@ class CachedProgramLangRepo(ProgramLangRepo):
     def _key_builder(self, username: str) -> str:
         return username
 
-    async def fetch_lang(self, username: str) -> dict[str, float]:
+    async def fetch_lang(self, username: str, limit: int) -> dict[str, int | float]:
         if data := await self.cache.get_from_cache(self._key_builder(username)):
             return data
 
-        data = await self.repository.get_languages(username)
+        data = await self.repository.get_languages(limit, username)
 
         await self.cache.record_in_cache(self._key_builder(username), data)
         return data
