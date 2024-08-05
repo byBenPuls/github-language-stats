ca279cc710766db207b5e0043744e9bd68f8c8ea
Ben Puls <mail@benpuls.ru>
2024-08-05 12:08:35 +0500

    add: tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,10 @@
-# tests
+from fastapi.testclient import TestClient
+from src.main import create_app as app
+
+client = TestClient(app())
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
