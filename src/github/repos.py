8d462392b1045f4d4a1a30ec9dd71af5b6bd5a84
Ben Puls <mail@benpuls.ru>
2024-08-06 14:25:14 +0500

    refactor: variable names

--- src/github/repos.py
+++ src/github/repos.py
@@ -11,30 +11,35 @@ logger = logging.getLogger("uvicorn.info")
 
 class ProgramLangRepo:
     def __init__(self) -> None:
-        self.github = GitHubHTTPClient()
+        self.github_http_client = GitHubHTTPClient()
 
     @staticmethod
-    def lang_sorter(langs: list, limit: int = 6) -> dict[str, int | float]:
+    def lang_sorter(langs: list, numbers_of_languages_limiter: int | None = 6) -> dict[str, int | float]:
+        """
+        :param langs: languages list
+        :param numbers_of_languages_limiter: Languages on the number of languages returned.
+        If the limit is `None`, all user languages will be returned.
+        """
         result = defaultdict(int)
 
         for language_response in langs:
-            for language, count in language_response.items():
-                result[language] += count
-        return dict(Counter(dict(result)).most_common(limit))
+            for name, count in language_response.items():
+                result[name] += count
+        return dict(Counter(dict(result)).most_common(numbers_of_languages_limiter))
 
     async def get_languages(self, limit: int, username: str) -> dict[str, int | float]:
         try:
-            repos = await self.github.get_repos(username)
+            repos = await self.github_http_client.get_all_repos_of_user(username)
 
             languages_list = await asyncio.gather(
                 *[
-                    self.github.get_languages_from_repo(repo["languages_url"])
+                    self.github_http_client.get_languages_from_repo(repo["languages_url"])
                     for repo in repos
                 ]
             )
         except (httpx.HTTPStatusError, httpx.ConnectError) as e:
             logger.info(e)
             return {}
-        langs = self.lang_sorter(list(languages_list), limit=limit)
+        langs = self.lang_sorter(list(languages_list), limit)
         logger.info(langs)
         return langs
