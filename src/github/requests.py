5f90e8dc880a31c3e39cc80bbd99076674d09a04
Ben Puls <mail@benpuls.ru>
2024-09-06 15:42:27 +0500

    add: custom raise_for_status

--- src/github/requests.py
+++ src/github/requests.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import httpx
 
+from src.exceptions import NotFoundUserError, RequestLimitError, TokenInvalidError
 from src.settings import settings
 
 
@@ -10,11 +11,23 @@ class GitHubHTTPClient:
         self.client = httpx.AsyncClient()
         self.api_token = settings.GITHUB_API_TOKEN
 
+    @staticmethod
+    def raise_for_status(response: httpx.Response) -> None:
+        match response.status_code:
+            case 200:
+                return
+            case 404:
+                raise NotFoundUserError("Username not found :(")
+            case 403 | 429:
+                raise RequestLimitError("Request limit exceeded :(")
+            case 401:
+                raise TokenInvalidError("Token invalid :(")
+
     async def get(self, url: str) -> Any:
         response = await self.client.get(
             url, headers={"Authorization": f"Bearer {self.api_token}"}
         )
-        response.raise_for_status()
+        self.raise_for_status(response)
         return response.json()
 
     async def get_all_repos_of_user(self, username: str) -> list[dict]:
